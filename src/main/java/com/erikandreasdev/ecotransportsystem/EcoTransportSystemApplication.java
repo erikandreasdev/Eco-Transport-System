843c8a40a2487630247a69cc9017c9db70e6ee18
Erik Andreas Barreto de Vera <erik.dvera+dev@proton.me>
2024-10-30 11:15:47 +0000

    init project

---
+++ src/main/java/com/erikandreasdev/ecotransportsystem/EcoTransportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.erikandreasdev.ecotransportsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTransportSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoTransportSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erikandreasdev/ecotransportsystem/EcoTransportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erikandreasdev.ecotransportsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTransportSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
