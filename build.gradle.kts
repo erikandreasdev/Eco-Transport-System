cc51376e47a11b72e33bd7d230f0f6a4ad7c8094
Erik Andreas Barreto de Vera <erik.dvera+dev@proton.me>
2024-10-30 11:34:19 +0000

    Add JPA, PostgreSQL, and Docker Compose dependencies
    Included dependencies for JPA and PostgreSQL to enable database interactions. Added Docker Compose dependency for container orchestration support. These changes enhance the application's scalability and ease of deployment.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,9 +25,12 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
